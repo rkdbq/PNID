49447abd08a1826b820b59c273b23f022ac4d656
Bonggu Kang <rkdbg11752@naver.com>
2023-03-18 23:39:33 +0900

    Delete IoU_calculator.py

--- rkdbq/postprocess/IoU_calculator.py
+++
@@ -1,7 +0,0 @@
-from shapely.geometry import Polygon
-
-a = Polygon([(1, 1), (5, 1), (5, 4), (1, 4)])
-b = Polygon([(1, 1), (5, 1), (5, 4), (1, 4)])
-
-print(a.intersection(b).area)
-print(a.union(b).area)
